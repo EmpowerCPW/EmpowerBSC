f319cb1818c20db41b6505e60ef285267498309a
Chris Walsh <chris.walsh@paramount.com>
2023-05-16 14:09:37 -0400

    Add project files.

---
+++ Empower_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Empower_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Empower_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("CDCC81AB-5676-4513-A37B-CC7107A2816D")]
+[assembly: AssemblyVersion("1.0.0.0")]
